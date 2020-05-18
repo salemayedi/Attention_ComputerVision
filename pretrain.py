75017d43f8fde4af13ec61ff446da9013f192363
salemayedi <salem.ayadi@itegia.de>
2020-05-19 01:42:09 +0200

    modified the types of the lists

--- pretrain.py
+++ pretrain.py
@@ -94,10 +94,10 @@ def main(args):
     train_acc_list = []
     val_loss_list = []
     val_acc_list = []
-    for epoch in range(10):
+    for epoch in range(1):
         print("Epoch {}".format(epoch))
-        train_loss, train_acc = train(train_loader, model, criterion, optimizer)
-        val_loss, val_acc = validate(val_loader, model, criterion)
+        train_loss, train_acc = train(train_loader, model, criterion, optimizer, device)
+        val_loss, val_acc = validate(val_loader, model, criterion, device)
 
         train_loss_list.append(train_loss)
         train_acc_list.append(train_acc)
@@ -112,9 +112,9 @@ def main(args):
             ######raise NotImplementedError("TODO: save model if a new best validation error was reached")
 
     pd.DataFrame({'train_loss':train_loss_list}).to_csv(args.plots_folder+'\\'+'train_loss.csv', index= False)
-    pd.DataFrame({'train_acc':train_acc_list}).to_csv(args.plots_folder+'\\'+'acc_loss.csv', index= False)
+    pd.DataFrame({'train_acc':train_acc_list}).to_csv(args.plots_folder+'\\'+'train_acc.csv', index= False)
     pd.DataFrame({'val_loss':val_loss_list}).to_csv(args.plots_folder+'\\'+'val_loss.csv', index= False)
-    pd.DataFrame({'val_acc':val_acc_list}).to_csv(args.plots_folder+'\\'+'acc_loss.csv', index= False)
+    pd.DataFrame({'val_acc':val_acc_list}).to_csv(args.plots_folder+'\\'+'val_acc.csv', index= False)
 
     save_fig (train_loss_list, 'train_loss')
     save_fig (train_acc_list, 'train_acc')
@@ -123,21 +123,24 @@ def main(args):
 
 
 # train one epoch over the whole training dataset. You can change the method's signature.
-def train(loader, model, criterion, optimizer):
+def train(loader, model, criterion, optimizer, device):
     #model.train()
     i = 0
     train_loss = 0
     train_acc = 0
     for batch_i, (data, target) in enumerate(loader):
         optimizer.zero_grad()
-        output = model(data)
+        output = model(data.to(device))
         loss = criterion(output, target)
         loss.backward()
         optimizer.step()
-        train_loss += round(loss.item(),5)
-        train_acc += accuracy(output, target)
+        train_loss += loss.mean().item()
+        #train_acc += accuracy(output, target)
+        train_acc += accuracy(output, target)[0].numpy()[0]
         i += 1
         print('new batch ',batch_i, ' with loss: ', round(loss.item(),3))
+        if (i== 50):
+            break
     train_acc = round((train_acc/i),3)
     
     return train_loss, train_acc
@@ -145,13 +148,21 @@ def train(loader, model, criterion, optimizer):
 
 
 # validation function. you can change the method's signature.
-def validate(loader, model, criterion):
+def validate(loader, model, criterion, device):
+    i = 0
+    val_loss = 0
+    val_acc = 0
     with torch.no_grad():        
-        for data,target in enumerate (loader):
-            output = model(data)
-            val_loss = criterion(output, target).mean()
-            val_acc = accuracy(output, target)
-
+        for batch_i, (data, target) in enumerate (loader):
+            print ('validation batch: ', batch_i)
+            output = model(data.to(device))
+            val_loss = criterion(output, target).mean().numpy()
+            val_acc += accuracy(output, target)[0].numpy()[0]
+            i += 1
+            if (i== 50):
+                break
+    val_acc = round((val_acc/i),3)
+    print('val_acc ', val_acc, ' val_loss ', val_loss)
     return val_loss, val_acc
     #########raise NotImplementedError("TODO: validation routine")
     # return mean_val_loss, mean_val_accuracy
