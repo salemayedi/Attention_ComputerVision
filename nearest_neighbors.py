0006bcae8a4181fba8dea8ef23025e3212d3186d
salemayedi <salem.ayadi@itegia.de>
2020-05-21 00:22:49 +0200

    nearest neighbors

--- nearest_neighbors.py
+++ nearest_neighbors.py
@@ -7,10 +7,14 @@ from torchvision.transforms import *
 from utils import check_dir
 from models.pretraining_backbone import ResNet18Backbone
 from data.pretraining import DataReaderPlainImg
+from sklearn.neighbors import NearestNeighbors
+from data.pretraining import DataReaderPlainImg, custom_collate
+import numpy as np
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
+    parser.add_argument('data_folder', type=str, help="folder containing the data (crops)")
     parser.add_argument('--weights-init', type=str,
                         default="")
     parser.add_argument("--size", type=int, default=256, help="size of the images to feed the network")
@@ -27,25 +31,34 @@ def parse_arguments():
 
 def main(args):
     # model
-    raise NotImplementedError("TODO: build model and load weights snapshot")
-
+    #raise NotImplementedError("TODO: build model and load weights snapshot")
+    # Device configuration
+    data_root = args.data_folder
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('#### This is the device used: ', device, '####')
+    model = ResNet18Backbone(pretrained=False).to(device) 
+    model.load_state_dict(torch.load(args.weights_init, map_location = device) , strict=False)
     # dataset
     val_transform = Compose([Resize(args.size), CenterCrop((args.size, args.size)), ToTensor()])
-    raise NotImplementedError("Load the validation dataset (crops), use the transform above.")
-
+    val_data = DataReaderPlainImg(os.path.join(data_root, str(args.size), "val"), transform=val_transform)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=1, shuffle=False, num_workers=2,
+                                             pin_memory=True, drop_last=True, collate_fn=custom_collate)
+    val_loader= val_loader.dataset
+    #raise NotImplementedError("Load the validation dataset (crops), use the transform above.")
+    print(val_loader)
     # choose/sample which images you want to compute the NNs of.
     # You can try different ones and pick the most interesting ones.
-    query_indices = []
+    query_indices = [5]
     nns = []
     for idx, img in enumerate(val_loader):
         if idx not in query_indices:
             continue
         print("Computing NNs for sample {}".format(idx))
-        closest_idx, closest_dist = find_nn(model, img, val_loader, 5)
-        raise NotImplementedError("TODO: retrieve the original NN images, save them and log the results.")
+        closest_idx, closest_dist, images_names = find_nn(model, img, val_loader, 5, device)
+        #raise NotImplementedError("TODO: retrieve the original NN images, save them and log the results.")
 
 
-def find_nn(model, query_img, loader, k):
+def find_nn(model, query_img, loader, k, device):
     """
     Find the k nearest neighbors (NNs) of a query image, in the feature space of the specified mode.
     Args:
@@ -57,7 +70,22 @@ def find_nn(model, query_img, loader, k):
         closest_idx: the indices of the NNs in the dataset, for retrieving the images
         closest_dist: the L2 distance of each NN to the features of the query image
     """
-    raise NotImplementedError("TODO: nearest neighbors retrieval")
+    output_network = []
+    loaded_data = torch.stack([x for i,x in enumerate(loader)])
+    idx = [i for i in range(len(loaded_data)) if torch.all(loaded_data[i].eq(query_img))][0]
+    list_images = loader.image_files
+    with torch.no_grad():
+        output_network = model(loaded_data)
+    
+    nbrs = NearestNeighbors(n_neighbors=k+1, algorithm='ball_tree', metric = 'euclidean').fit(output_network.numpy())
+    distances, indices = nbrs.kneighbors(output_network)
+
+    #import pdb; pdb.set_trace()
+    images_names = [ list_images[indices[idx][i]] for i in range(len(indices[idx]))]
+    
+    return indices[idx].tolist(), distances[idx].tolist(), images_names
+
+    #raise NotImplementedError("TODO: nearest neighbors retrieval")
     # return closest_idx, closest_dist
 
 
